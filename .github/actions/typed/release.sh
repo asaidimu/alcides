878c4e967d4d34b87ac861449b84d0295f909f58
saidimu <47994458+asaidimu@users.noreply.github.com>
2022-02-10 00:01:11 +0300

    ci: fix typo

--- .github/actions/typed/release.sh
+++ .github/actions/typed/release.sh
@@ -5,7 +5,7 @@ export GITHUB_TOKEN="${INPUT_GH_TOKEN}"
 
 git config --global user.email "47994458+asaidimu@users.noreply.github.com"
 git config --global user.name "saidimu"
-git config --global credential.helper -file "store --file .credentials"
+git config --global credential.helper "store --file .credentials"
 
 echo "https://asaidimu:${INPUT_GIT_TOKEN}@github.com" > ".credentials"
 
