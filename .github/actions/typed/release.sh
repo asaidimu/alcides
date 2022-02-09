0e41a91d4b1788aa82a3466a9af2fb5d3c4777a1
saidimu <47994458+asaidimu@users.noreply.github.com>
2022-02-09 23:05:09 +0300

    ci: added configs for git

--- .github/actions/typed/release.sh
+++ .github/actions/typed/release.sh
@@ -2,5 +2,8 @@
 set -eu
 
 export GITHUB_TOKEN="${INPUT_GH_TOKEN}"
+git config --global user.email "47994458+asaidimu@users.noreply.github.com"
+git config --global user.name "saidimu"
+
 yarn
 yarn build:types
