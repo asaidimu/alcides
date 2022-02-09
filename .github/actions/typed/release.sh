1a7072199b2192aee31094b23c5b0d1e19a81e32
saidimu <47994458+asaidimu@users.noreply.github.com>
2022-02-09 23:48:19 +0300

    ci: add credentials for git

--- .github/actions/typed/release.sh
+++ .github/actions/typed/release.sh
@@ -7,7 +7,7 @@ git config --global user.email "47994458+asaidimu@users.noreply.github.com"
 git config --global user.name "saidimu"
 git config --global credential.helper -file "store --file ~/.credentials"
 
-echo "https://asaidimu:${INPUT_GIT_TOKEN}@mygithost" > "~/.credentials"
+echo "https://asaidimu:${INPUT_GIT_TOKEN}@github.com" > "~/.credentials"
 
 yarn
 yarn build:types
