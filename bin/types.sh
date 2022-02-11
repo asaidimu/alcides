0f7e982b5c39cb1a310b3e832ef642186581c697
saidimu <47994458+asaidimu@users.noreply.github.com>
2022-02-11 20:49:13 +0300

    chore: fix typo in type definitions

--- bin/types.sh
+++ bin/types.sh
@@ -22,8 +22,7 @@ cat > "$TARGET/index.d.ts" <<EOF
 // Project: https://github.com/asaidimu/alcides
 // Definitions by: saidimu <https://github.com/asaidimu>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 4.6
-
+// TypeScript Version: 3
 EOF
 
 cat types/index.d.ts >> "$TARGET/index.d.ts"
