f80e9b70d273b3b23933e6935ea4fa52b517764e
Saidimu <47994458+asaidimu@users.noreply.github.com>
2022-02-26 02:21:07 +0300

    chore: remove config from vcs

--- .alcides.js
+++
@@ -1,8 +0,0 @@
-export default {
-    include: ['dist/tests/**/*.js'],
-    timeout: 5000,
-    watch: process.env.NODE_ENV == 'development',
-    files: ['dist'],
-    parallel: false,
-    verbose: false,
-}
